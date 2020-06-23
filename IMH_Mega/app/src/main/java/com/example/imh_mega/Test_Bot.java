83a3e2df7bc465d2066f69b76184b1f80380f6ed
Andoy0823 <60495923+Andoy0823@users.noreply.github.com>
2020-06-23 23:23:15 +0800

    Firebase Connected - R.T.D

---
+++ IMH_Mega/app/src/main/java/com/example/imh_mega/Test_Bot.java
@@ -0,0 +1,14 @@
+package com.example.imh_mega;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Test_Bot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test__bot);
+    }
+}
