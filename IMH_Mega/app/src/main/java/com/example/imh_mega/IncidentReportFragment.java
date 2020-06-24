996ff61862ffea95788a7729c5c85a44675d14f4
RedStark17 <joshuajovenir@gmail.com>
2020-06-24 22:48:36 +0800

    Homepage and Reports
    Added Homepage UI and Reports mini fragments.

--- IMH_Mega/app/src/main/java/com/example/imh_mega/IncidentReportFragment.java
+++ IMH_Mega/app/src/main/java/com/example/imh_mega/IncidentReportFragment.java
@@ -11,18 +11,19 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 
-public class ReportFragment extends Fragment {
+public class IncidentReportFragment extends Fragment{
 
+    NavController navController;
+    Spinner spinnerReportType;
 
-
-    public ReportFragment() {
+    public IncidentReportFragment() {
         // Required empty public constructor
     }
 
@@ -30,24 +31,34 @@ public class ReportFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report, container, false);
+        return inflater.inflate(R.layout.fragment_incident_report, container, false);
     }
 
-    /*
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+
         spinnerReportType = view.findViewById(R.id.spinnerReportTypeID);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.reportTypes, android.R.layout.simple_spinner_item);
-
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
         spinnerReportType.setAdapter(adapter);
-        spinnerReportType.setOnItemSelectedListener(this);
+        spinnerReportType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+                String text = parent.getItemAtPosition(position).toString();
+                navController = Navigation.findNavController(view);
+                if (text.equals("Location History Report")){
+                    navController.navigate(R.id.action_incidentReportFragment_to_locationHistoryReportFragment);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
     }
-
-     */
 
 }
---
+++ IMH_Mega/app/src/main/java/com/example/imh_mega/LocationHistoryReportFragment.java
@@ -0,0 +1,62 @@
+package com.example.imh_mega;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+
+public class LocationHistoryReportFragment extends Fragment {
+
+    NavController navController;
+    Spinner spinnerLocReportType;
+
+    public LocationHistoryReportFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_location_history_report, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+        spinnerLocReportType = view.findViewById(R.id.spinnerLocReportTypeID);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.reportTypes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerLocReportType.setAdapter(adapter);
+        spinnerLocReportType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+                String text = parent.getItemAtPosition(position).toString();
+                navController = Navigation.findNavController(view);
+                if (text.equals("Incident Report")){
+                    navController.navigate(R.id.action_locationHistoryReportFragment_to_incidentReportFragment);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+}
